da5e1c0b12ef8ef55ea413231f37c3e59509e4c7
JV <jv@lewe-hoffbauer.de>
2023-06-20 13:11:29 +0000

    Add functionality to load from checkpoints to merge script

--- merge_peft_adapter.py
+++ merge_peft_adapter.py
@@ -1,17 +1,24 @@
 from dataclasses import dataclass, field
+from os.path import join
 from typing import Optional
 
 import peft
 import torch
-from peft import PeftConfig, PeftModel
+from peft import (
+    LoraConfig,
+    PeftConfig,
+    PeftModel,
+    TaskType,
+    get_peft_model,
+    set_peft_model_state_dict,
+)
 from peft.utils import _get_submodules
-from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer, HfArgumentParser
-
-
-DEFAULT_PAD_TOKEN = "[PAD]"
-DEFAULT_EOS_TOKEN = "</s>"
-DEFAULT_BOS_TOKEN = "</s>"
-DEFAULT_UNK_TOKEN = "</s>"
+from transformers import (
+    AutoConfig,
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    HfArgumentParser,
+)
 
 
 @dataclass
@@ -20,23 +27,60 @@ class ScriptArguments:
     The name of the Casual LM model we wish to fine with PPO
     """
 
-    adapter_model_name: Optional[str] = field(default=None, metadata={"help": "the model name"})
-    base_model_name: Optional[str] = field(default=None, metadata={"help": "the model name"})
-    output_name: Optional[str] = field(default=None, metadata={"help": "the model name"})
+    adapter_model_name: Optional[str] = field(default=None)
+    checkpoint_dir: Optional[str] = field(default=None)
+    base_model_name: Optional[str] = field(default=None)
+    output_name: Optional[str] = field(default=None)
 
 
 parser = HfArgumentParser(ScriptArguments)
 script_args = parser.parse_args_into_dataclasses()[0]
-assert script_args.adapter_model_name is not None, "please provide the name of the Adapter you would like to merge"
+assert (
+    script_args.adapter_model_name is not None or script_args.checkpoint_dir is not None
+), "please provide an adapter or checkpoint"
 assert script_args.base_model_name is not None, "please provide the name of the Base model"
 assert script_args.output_name is not None, "please provide the output name of the merged model"
 
-peft_config = PeftConfig.from_pretrained(script_args.adapter_model_name)
-model = AutoModelForCausalLM.from_pretrained(script_args.base_model_name, return_dict=True)
+
+if script_args.checkpoint_dir is not None:
+    model = AutoModelForCausalLM.from_pretrained(script_args.base_model_name)
+
+    # This needs to match the training configuration _exactly_.
+    peft_config = LoraConfig(
+        task_type=TaskType.SEQ_CLS,
+        inference_mode=False,
+        r=8,
+        lora_alpha=32,
+        lora_dropout=0.1,
+    )
+    model = get_peft_model(model, peft_config)
+
+    # Load the checkpoint
+    full_state_dict = torch.load(join(script_args.checkpoint_dir, "pytorch_model.bin"), map_location="cpu")
+    set_peft_model_state_dict(model, full_state_dict)
+
+    model.eval()
+
+elif script_args.adapter_model_name is not None:
+    peft_config = PeftConfig.from_pretrained(script_args.adapter_model_name)
+    model = AutoModelForCausalLM.from_pretrained(script_args.base_model_name, return_dict=True)
+
+    # Load the Lora model
+    model = PeftModel.from_pretrained(model, script_args.adapter_model_name)
+    model.eval()
+
+
 tokenizer = AutoTokenizer.from_pretrained(script_args.base_model_name)
+
+
+# For Llama, we need to set the EOS, BOS, and UNK tokens
 config = AutoConfig.from_pretrained(script_args.base_model_name)
 architecture = config.architectures[0]
 if "Llama" in architecture:
+    DEFAULT_PAD_TOKEN = "[PAD]"
+    DEFAULT_EOS_TOKEN = "</s>"
+    DEFAULT_BOS_TOKEN = "</s>"
+    DEFAULT_UNK_TOKEN = "</s>"
     print("Setting EOS, BOS, and UNK tokens for LLama tokenizer")
     tokenizer.add_special_tokens(
         {
@@ -47,9 +91,6 @@ if "Llama" in architecture:
         }
     )
 
-# Load the Lora model
-model = PeftModel.from_pretrained(model, script_args.adapter_model_name)
-model.eval()
 
 key_list = [key for key, _ in model.base_model.model.named_modules() if "lora" not in key]
 for key in key_list:
@@ -63,4 +104,5 @@ model = model.base_model.model
 
 model.save_pretrained(f"{script_args.output_name}")
 tokenizer.save_pretrained(f"{script_args.output_name}")
-# model.push_to_hub(f"{script_args.output_name}", use_temp_dir=False)
+
+print(f"Saved model to {script_args.output_name}")
