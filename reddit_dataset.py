ac1f2bc8a6a68c24ba610d6a4147da5a86abb372
JV <jv@lewe-hoffbauer.de>
2023-06-24 19:14:26 +0000

    Remove 'ELI5' prefix from titles

--- reddit_dataset.py
+++ reddit_dataset.py
@@ -1,10 +1,23 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
+import re
+
 import datasets as ds
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
 DATASETS_BASE_PATH = "/scratch1/jhoff"
+
+
+TITLE_STARTS_TO_REMOVE = [
+    "LI5",
+    "ELI5",
+    "ELI5:",
+    "\\[ELI5\\]:",
+    "ELI5 -",
+    "\\(ELI5\\)",
+]
+TITLE_STARTS_TO_REMOVE = [re.compile(f"^{start}") for start in TITLE_STARTS_TO_REMOVE]
 
 
 def binary_comparison(answers):
@@ -49,6 +62,12 @@ def preprocess_pair_generation(examples):
     return new_examples
 
 
+def preprocess_remove_title_start(example):
+    for start in TITLE_STARTS_TO_REMOVE:
+        example["submission_title"] = re.sub(start, "", example["submission_title"]).strip()
+    return example
+
+
 def load_reddit_dataset(split=None, pairs=False):
     if split is None:
         datasets = ds.DatasetDict(
@@ -68,6 +87,9 @@ def load_reddit_dataset(split=None, pairs=False):
     # Trick to enable caching: Save and load the dataset to make sure it has a cache file
     datasets.save_to_disk(f"{DATASETS_BASE_PATH}/reddit_dataset_cached")
     datasets = ds.load_from_disk(f"{DATASETS_BASE_PATH}/reddit_dataset_cached")
+
+    # Remove "ELIF" from the beginning of the title
+    datasets = datasets.map(preprocess_remove_title_start)
 
     if pairs:
         datasets = datasets.map(preprocess_pair_generation, batch_size=10, batched=True)
