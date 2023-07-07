1e5ff2a3cfef9bd48fbf00ead8c0cdda787badeb
JV <jv@lewe-hoffbauer.de>
2023-07-07 18:09:23 +0000

    Add script to run commands in docker more easily

---
+++ run_in_docker.sh
@@ -0,0 +1,16 @@
+set -eux
+
+CUDA_DEVICE=$1
+COMMAND=$2
+
+echo "Running command '$COMMAND' on GPU $CUDA_DEVICE"
+
+docker build . -t redditqa
+
+docker run \
+    --gpus device=$CUDA_DEVICE \
+    --mount type=bind,source=/scratch1/jhoff,target=/scratch1/jhoff \
+    --mount type=bind,source=/home/jhoffbauer/.cache/huggingface,target=/hf_cache \
+    --env WANDB_API_KEY=$WANDB_API_KEY \
+    -it redditqa \
+    $COMMAND 2>&1 | tee -a logs/output$(date +"%d-%m-%Y").txt
