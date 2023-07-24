2150b6a24161f9259f3e33dea1f4ae9ab6dedc07
JV <jv@lewe-hoffbauer.de>
2023-07-24 13:58:25 +0000

    Update docker script to run in other environments

--- run_in_docker.sh
+++ run_in_docker.sh
@@ -2,6 +2,7 @@ set -eux
 
 CUDA_DEVICE=$1
 COMMAND=$2
+SCRATCH_PATH=$3
 
 echo "Running command '$COMMAND' on GPU $CUDA_DEVICE"
 
@@ -9,8 +10,9 @@ docker build . -t redditqa --build-arg HUGGINGFACE_TOKEN=$HUGGINGFACE_TOKEN
 
 docker run \
     --gpus device=$CUDA_DEVICE \
-    --mount type=bind,source=/scratch1/jhoff,target=/scratch1/jhoff \
-    --mount type=bind,source=/home/jhoffbauer/.cache/huggingface,target=/hf_cache \
+    --mount type=bind,source=$SCRATCH_PATH,target=/scratch1/jhoff \
+    #--mount type=bind,source=/scratch1/jhoff,target=/scratch1/jhoff \
+    #--mount type=bind,source=/home/jhoffbauer/.cache/huggingface,target=/hf_cache \
     --env WANDB_API_KEY=$WANDB_API_KEY \
     -it redditqa \
     $COMMAND 2>&1 | tee -a logs/output_$(date +"%d-%m-%Y").txt
