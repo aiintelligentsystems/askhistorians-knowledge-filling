0d2700e14ca4b46ffe1853d6b4140376c7b3dda7
JV <jv@lewe-hoffbauer.de>
2023-07-24 16:00:58 +0000

    Fix typo in run_in_docker.sh

--- run_in_docker.sh
+++ run_in_docker.sh
@@ -11,8 +11,6 @@ docker build . -t redditqa --build-arg HUGGINGFACE_TOKEN=$HUGGINGFACE_TOKEN
 docker run \
     --gpus device=$CUDA_DEVICE \
     --mount type=bind,source=$SCRATCH_PATH,target=/scratch1/jhoff \
-    #--mount type=bind,source=/scratch1/jhoff,target=/scratch1/jhoff \
-    #--mount type=bind,source=/home/jhoffbauer/.cache/huggingface,target=/hf_cache \
     --env WANDB_API_KEY=$WANDB_API_KEY \
     -it redditqa \
     $COMMAND 2>&1 | tee -a logs/output_$(date +"%d-%m-%Y").txt
