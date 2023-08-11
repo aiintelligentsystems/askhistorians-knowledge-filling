29b9a273272000e505e601ea1860d956336a62c6
JV <jv@lewe-hoffbauer.de>
2023-08-11 13:22:22 +0200

    Minor fixes in docker script

--- run_in_docker.sh
+++ run_in_docker.sh
@@ -2,7 +2,6 @@ set -eux
 
 CUDA_DEVICE=$1
 COMMAND=$2
-SCRATCH_PATH=$3
 
 echo "Running command '$COMMAND' on GPU $CUDA_DEVICE"
 
@@ -10,7 +9,8 @@ docker build . -t redditqa --build-arg HUGGINGFACE_TOKEN=$HUGGINGFACE_TOKEN
 
 docker run \
     --gpus device=$CUDA_DEVICE \
-    --mount type=bind,source=$SCRATCH_PATH,target=/scratch1/jhoff \
+    --mount type=bind,source=/scratch1/jhoff,target=/scratch1/jhoff \
+    --mount type=bind,source=/home/jhoffbauer/.cache/huggingface,target=/hf_cache \
     --env WANDB_API_KEY=$WANDB_API_KEY \
     -it redditqa \
     $COMMAND 2>&1 | tee -a logs/output_$(date +"%d-%m-%Y").txt
