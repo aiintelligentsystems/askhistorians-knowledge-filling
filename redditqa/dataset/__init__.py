4d262e5ea59e6c5d1e234a0351efd04684c5099a
JV <jv@lewe-hoffbauer.de>
2023-08-11 13:20:46 +0200

    Retain scores in pairwise dataset and cleanup

--- redditqa/dataset/__init__.py
+++ redditqa/dataset/__init__.py
@@ -17,10 +17,21 @@ def binary_comparison(answers):
 
     for i in range(len(answers) - 1):
         for j in range(i + 1, len(answers)):
-            if answers[i]["answer_score"] > answers[j]["answer_score"]:
-                pairs.append((answers[i]["answer_body"], answers[j]["answer_body"]))
-            elif answers[i]["answer_score"] < answers[j]["answer_score"]:
-                pairs.append((answers[j]["answer_body"], answers[i]["answer_body"]))
+            # Get scores and data
+            answer_i = answers[i]
+            answer_j = answers[j]
+            answer_score_i = answer_i["answer_score"]
+            answer_score_j = answer_j["answer_score"]
+            answer_body_i = answers[i]["answer_body"]
+            answer_body_j = answers[j]["answer_body"]
+            data_i = {"score": answer_score_i, "body": answer_body_i}
+            data_j = {"score": answer_score_j, "body": answer_body_j}
+
+            # Create pair
+            if answer_score_i > answer_score_j:
+                pairs.append((data_i, data_j))
+            elif answer_score_i < answer_score_j:
+                pairs.append((data_j, data_i))
     return pairs
 
 
@@ -30,26 +41,30 @@ def preprocess_pair_generation(examples):
     MAX_PAIRS_PER_QUESTION = 10
     n_samples = len(examples["answer_link_id"])
 
-    # initialize empty lists for new samples
-    new_examples = {"question_title": [], "response_j": [], "response_k": []}
+    # Initialize empty lists for new samples
+    new_examples = {"question_title": [], "response_j": [], "response_k": [], "score_j": [], "score_k": []}
+
+    # Overwrite all other keys
     for key in examples:
         new_examples[key] = []
 
     for sample_id in range(n_samples):
-        # get pairs where first is always the better one
+        # Get pairs where first is always the better one
         pairs = binary_comparison(examples["answers"][sample_id])
 
-        # sample if we get more pairs than maximum
+        # Sample if we get more pairs than maximum
         if len(pairs) > MAX_PAIRS_PER_QUESTION:
             indices = np.random.choice(list(range(len(pairs))), MAX_PAIRS_PER_QUESTION, replace=False)
             pairs = [pairs[i] for i in indices]
 
-        # construct the samples
+        # Construct the samples
         for pair in pairs:
             for key in examples:
                 new_examples[key].append(examples[key][sample_id])
-            new_examples["response_j"].append(pair[0])
-            new_examples["response_k"].append(pair[1])
+            new_examples["response_j"].append(pair[0]["body"])
+            new_examples["response_k"].append(pair[1]["body"])
+            new_examples["score_j"].append(pair[0]["score"])
+            new_examples["score_k"].append(pair[1]["score"])
     return new_examples
 
 
