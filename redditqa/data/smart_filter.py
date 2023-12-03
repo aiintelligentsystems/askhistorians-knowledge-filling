def2a73c93eb18ce7be26ce3ba72bf0d37ff0f57
Sylwester Sawicki <33114066+sawicki-smk@users.noreply.github.com>
2023-12-03 11:50:13 +0000

    [CHG] smart filter adaptions to yes/no and wiki rules

--- redditqa/data/smart_filter.py
+++ redditqa/data/smart_filter.py
@@ -1,9 +1,43 @@
+from dataclasses import dataclass
 import transformers
+from collections import namedtuple
+import torch
+import numpy as np
+
+# namedtuple or dataclass not PyArrow serializable
+
+# @dataclass
+# class GradedOutput:
+#     token_id: torch.Tensor
+#     token_str: str
+#     probability: float
+
+# graded_output = namedtuple('GradedOutput', 'token_id token_str probability')
 
 
-def question_filter(ds_item: dict, pipeline: transformers.pipeline, verbose=False):
+def generation_pipeline(prompt: str, model, tokenizer):
+    inputs = tokenizer(prompt, return_tensors="pt")
+    inputs.to(model.device)
+
+    outputs = model.generate(**inputs, max_new_tokens=1, return_dict_in_generate=True, output_scores=True)
+    transition_scores = model.compute_transition_scores(
+        outputs.sequences, outputs.scores, normalize_logits=True
+    )
+
+    input_length = inputs.input_ids.shape[1]
+    generated_tokens = outputs.sequences[:, input_length:]
+
+    grading_dict = dict(graded_output=[])
+
+    for tok, score in zip(generated_tokens[0], transition_scores[0]):
+        # namedtuple or dataclass not PyArrow serializable
+        grading_dict['graded_output'].append(dict(token_id=tok.cpu(), token_str=tokenizer.decode(tok), probability=np.exp(score.cpu().numpy())))
+    return grading_dict
+
+
+def question_grading_map(ds_item: dict, pipeline: transformers.pipeline, verbose=False):
     """
-    Returns True if question is graded as good by llm, otherwise False.
+    Grades a question with a llm.
     """
 
     query = """
@@ -12,38 +46,41 @@ def question_filter(ds_item: dict, pipeline: transformers.pipeline, verbose=Fals
 
     Good questions 
 
-    - cannot be answered in one sentence
+    - should be about an event or person or culture in history
+    - may also be about historical method (e.g. “How should we deal with the biases in primary sources?”)
     - do not contain a personal reference
     - are not suggestive questions 
     - do not ask for book recommendations
     - do not contain hateful statements
+    - are not poll-type questions (e.g. "Who was the most influential person in history?")
 
     Here are some examples how to grade questions:
-        
-    Question: What caused the Wall Street Crash of 1929? 
-    Answer: good question
 
-    Question: Wednesday AMA: Magic, Alchemy, and the Occult 
-    Answer: bad question
+    ***Examples***
+    Is the following question a good question (Answer with yes/no)? What caused the Wall Street Crash of 1929? 
+    yes
 
-    Question: How did people do math with Roman Numerals?
-    Answer: good question
+    Is the following question a good question (Answer with yes/no)? Wednesday AMA: Magic, Alchemy, and the Occult 
+    no
 
-    Question: When does one become a historian?
-    Answer: bad question
+    Is the following question a good question (Answer with yes/no)? What were the consequences for the British in choosing to hold on to Northern Ireland after World War I?
+    yes
 
-    Question: How much of a threat was Ivan VI to Catherine the Great's reign as empress?
-    Answer: good question
+    Is the following question a good question (Answer with yes/no)? When does one become a historian?
+    no
 
-    Question: I need some books on the Asian continent in general.
-    Answer: bad question
+    Is the following question a good question (Answer with yes/no)? How much of a threat was Ivan VI to Catherine the Great's reign as empress?
+    yes
 
-
-    Classify the following question into one of the two classes: ['good question', 'bad question']:
+    Is the following question a good question (Answer with yes/no)? I need some books on the Asian continent in general.
+    no
+    ***Examples end***
+    
+    Is the following question a good question (Answer with yes/no)? 
     """
 
     question_title = ds_item['question_title']
-    question_text = f"""Question: {question_title}"""
+    question_text = f"""{question_title}\n"""
     messages = [
         {
             "role": "system",
@@ -55,21 +92,21 @@ def question_filter(ds_item: dict, pipeline: transformers.pipeline, verbose=Fals
         },
     ]
     prompt = pipeline.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
-    outputs = pipeline(prompt, max_new_tokens=50, do_sample=True, temperature=0.2, top_k=50, top_p=0.95, return_full_text=False)
-    output = outputs[0]["generated_text"]
+    
+    generation_out = generation_pipeline(prompt, pipeline.model, pipeline.tokenizer)
     if verbose:
-        print("#" * 50)
-        print(question_text)
-        print("---" * 30)
-        print(output)
-        print("#" * 50)
+        print(generation_out)
+    ds_item.update(generation_out)
+    return ds_item
 
-    return output.strip().startswith("Answer: good question")
+def question_filter(ds_item: dict, accepted_token_str=[], acceptance_thresh=0.2):
+    """Filters questions which do not contain `accepted_token_str` in grading with a probability of at least `acceptance_thresh`."""
+    output_grading: list[dict] = ds_item['graded_output']
+    return any(output["probability"] >= acceptance_thresh for output in output_grading if output["token_str"].lower() in accepted_token_str)
 
-
-def answer_filter(ds_item: dict, pipeline: transformers.pipeline, verbose=False):
+def answer_grading_map(ds_item: dict, pipeline: transformers.pipeline, verbose=False):
     """
-    Returns dataset item with answers which were graded as good answers by an llm.
+    Grades a answer with a llm.
     """
 
     answer_query = """
@@ -78,39 +115,37 @@ def answer_filter(ds_item: dict, pipeline: transformers.pipeline, verbose=False)
 
     Good answers 
 
-    - are not yes or no answers
-    - do not contain too many personal preferences
+    - should be in-depth, comprehensive, accurate, and based off of good quality sources
+    - provides the necessary context and complexity that the given topic calls for, going beyond a simple cursory overview
+    - do not contain personal anecdotes
+    - do not contain suppositions and personal opinions
     - are not suggestive 
     - do not contain hateful statements
-    - can contain references
 
     Here are some examples how to grade answers:
-        
-    Question: Did allied soldiers defect to Germany/ The Axis powers during WWII?
-    Answer: It was much more common on the Eastern Front, with significant numbers of former Red Army soldiers and civilians from the occupied areas in the east coming over to the German side.  Some of this was a byproduct of an earlier era, with many people in certain regions feeling as though they were occupied by the Soviets to begin with.
-    Grade: good answer
 
-    Question: Why did Soviet invade Afghanistan in 1979?
-    Answer: If you're a student, wouldn't this perhaps be a good thing to research on your own instead of letting other people do the work for you? Or is this unrelated to school?
-    Grade: bad answer
+    ***Examples start***
+    Given the question: Did allied soldiers defect to Germany/ The Axis powers during WWII?
+    Is the following answer a good answer (Answer with yes/no)? It was much more common on the Eastern Front, with significant numbers of former Red Army soldiers and civilians from the occupied areas in the east coming over to the German side.  Some of this was a byproduct of an earlier era, with many people in certain regions feeling as though they were occupied by the Soviets to begin with.
+    yes
 
-    Question: Do we know of any system of sustained diplomatic relations/interactions between the Hittite Empire and Achaea?
-    Answer: We know of a few other contacts between the two states, but not enough to show decisively that there were *sustained* diplomatic relations. In the Tawagalawa letter the Hittite king is clearly treading very carefully, because he wants to avoid offending the Ahhiyawan king, and apologises extensively for earlier aggression (and mentions previous apologies). The Ahhiyawan actions in sheltering a known rebel, Piyamaradu, were not exactly conciliatory. Moreover, we know that Miletos changed hands a couple of times between Ahhiyawa and the Hittites, not necessarily because of wars, but perhaps because of warlords (like Piyamaradu) stirring up trouble. But we know of no treaties or wars as such. So it seems safe to say that the Hittites and Ahhiyawans had an uneasy relationship, but no mortal enmity. They seem to have traded with one another: certainly Mycenaean artefacts (which may or may not be Ahhiyawan) found their way into Anatolia. I'm not aware that trade going the other way was extensive, though. There's a book that very conveniently contains all the documents relating to Ahhiyawa, *The Ahhiyawa Texts*, edited by Beckman, Cline, and Bryce (2012). In it you'll find all the documents relating to Ahhiyawa, including one very tantalising letter from the Ahhiyawan king to the Hittites (but, infuriatingly, it doesn't name the Ahhiyawan king! -- that would have been invaluable for all sorts of reasons, both historical and linguistic). It is not 100% certain that Ahhiyawa = "Achaia", though it is of course very likely. If you read *The Ahhiyawa Texts* you'll see one much later reference to Ahhiyawa in a region much further east, in SE Turkey; it's difficult to decode the implications of this.
-    Grade: good answer
+    Given the question: Why did Soviet invade Afghanistan in 1979?
+    Is the following answer a good answer (Answer with yes/no)? If you're a student, wouldn't this perhaps be a good thing to research on your own instead of letting other people do the work for you? Or is this unrelated to school?
+    no
 
-    Question: Meaning of Town Suffixes?
-    Answer: Sorry, we don't allow [throughout history questions]([LINK]  These tend to produce threads which are collections of trivia, not the in-depth discussions about a particular topic we're looking for.  If you have a specific question about a historical event or period or person, please feel free to re-compose your question and submit it again. Alternatively, questions of this type can be directed to more appropriate subreddits, such as /r/asklinguistics, /r/linguistics (NOTE: do not create a separate post; use the stickied Q&amp;A post), /r/history or /r/askhistory.
-    Grade: bad answer
+    Given the question: Meaning of Town Suffixes?
+    Is the following answer a good answer (Answer with yes/no)? Sorry, we don't allow [throughout history questions]([LINK]  These tend to produce threads which are collections of trivia, not the in-depth discussions about a particular topic we're looking for.  If you have a specific question about a historical event or period or person, please feel free to re-compose your question and submit it again. Alternatively, questions of this type can be directed to more appropriate subreddits, such as /r/asklinguistics, /r/linguistics (NOTE: do not create a separate post; use the stickied Q&amp;A post), /r/history or /r/askhistory.
+    no
+    ***Examples end***
 
-    Classify the following answer into one of the two classes: ['good answer', 'bad answer']:
+    Grade the answers given (Answer with yes/no)
     """
 
     question_title = ds_item['question_title']
     answers = ds_item['answers']
-    prompts, new_answers = [], []
 
     for answer in answers:
-        question_text = f"""Question: {question_title}\nAnswer: {answer['answer_body']}\n\Please clearly state if this is a good answer or a bad answer, give no explanations."""
+        question_text = f"""Given the question: {question_title}\nIs the following answer a good answer (Answer with yes/no)? {answer['answer_body']}\n"""
         messages = [
             {
                 "role": "system",
@@ -122,21 +157,19 @@ def answer_filter(ds_item: dict, pipeline: transformers.pipeline, verbose=False)
             },
         ]
         prompt = pipeline.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
-        prompts.append(prompt)
-
-    outputs = pipeline(prompts, max_new_tokens=50, do_sample=True, temperature=0.2, top_k=50, top_p=0.95, return_full_text=False)
-
-    for answer, output in zip(answers, outputs):
+        generation_out = generation_pipeline(prompt, pipeline.model, pipeline.tokenizer)
         if verbose:
-            print("#" * 50)
-            print(question_title)
-            print("---" * 30)
-            print(answer['answer_body'])
-            print("---" * 30)
-            print(output[0]["generated_text"].strip())
-            print("#" * 50)
+            print(generation_out)
+        answer.update(generation_out)
+    return ds_item
 
-        if "good answer" in output[0]["generated_text"].strip()[:100].lower():
+def answer_filter(ds_item: dict, accepted_token_str=[], acceptance_thresh=0.2):
+    """Filters answers which do not contain `accepted_token_str` in grading with a probability of at least `acceptance_thresh`."""
+    new_answers = []
+    for answer in ds_item["answers"]:
+        output_grading: list[dict] = answer['graded_output']
+        if any(output["probability"] >= acceptance_thresh for output in output_grading if output["token_str"].lower() in accepted_token_str):
             new_answers.append(answer)
-    ds_item['answers'] = new_answers
+
+    ds_item["answers"] = new_answers
     return ds_item
